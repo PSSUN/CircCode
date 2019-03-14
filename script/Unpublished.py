0e95c85036120921e4b3f6e67f4e2cf6149bc284
Peisen Sun <sps@snnu.edu.cn>
2019-03-14 15:37:32 +0800

    Update Unpublished.py

--- script/Unpublished.py
+++ script/Unpublished.py
@@ -1,4 +1,7 @@
 # This software is unpublised
 # We are here to publish the code for editorial review.
-# Please respect our results。
+# Please respect our results
 # Thank you! 
+
+# Copyright: Lilab@SNNU
+# E-mail : sps@snnu.edu.cn
