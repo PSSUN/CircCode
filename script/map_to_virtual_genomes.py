6402dba1e6f2b3e5263c3e7b493691b86c14b365
Peisen Sun <sps@snnu.edu.cn>
2019-05-30 20:43:51 +0800

    Update map_to_virtual_genomes.py

--- script/map_to_virtual_genomes.py
+++ script/map_to_virtual_genomes.py
@@ -1,4 +1,4 @@
-#usr/bin/python3
+#/usr/bin/python3
 
 import time
 import subprocess
@@ -12,7 +12,7 @@ from multiprocessing import Pool
 
 # make index of genome and ribo-seq reads
 def make_index(genome, ribosome,tmp_file_location):
-
+    
     # genomename = str(genome).split('/')[-1].split('.')[0]
     ribo_name = str(ribosome).split('/')[-1].split('.')[0]
     subprocess.call('bowtie-build {} {}/{}'
@@ -21,9 +21,9 @@ def make_index(genome, ribosome,tmp_file_location):
     subprocess.call('bowtie2-build {} {}/newGenome.fa'
                     .format(genome, tmp_file_location),
                     shell=True, stdout=False)
-    print('-' * 50)
+    print('-' * 100)
     print(get_time(), 'Make index successfully!')
-    print('-' * 50)
+    print('-' * 100)
 
 
 def deal_raw_data(genome, raw_read, ribosome, thread, trimmomatic, riboseq_adapters, tmp_file_location):
@@ -31,34 +31,36 @@ def deal_raw_data(genome, raw_read, ribosome, thread, trimmomatic, riboseq_adapt
     read_name = raw_read.split('/')[-1].split('.')[-2]
     ribo_name = str(ribosome).split('/')[-1].split('.')[0]
     without_rrna_reads = read_name+'.clean.without.rRNA.fastq'
+    print('-' * 100)
     print(get_time(), 'Loading reads form', raw_read)
-
+    print('-' * 100)
     # Transform sra to fastq format
     subprocess.call('fastq-dump {} -O {}'
                     .format(raw_read, tmp_file_location),
                     shell=True)
-
+    
     # Filter out low quality reads by Trimmomatic
     subprocess.call('java -jar {} SE -phred33 '
                     '{} {} ILLUMINACLIP:{}:2:30:10 '
                     'LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:16'
                     .format(trimmomatic, tmp_file_location+'/'+read_name+'.fastq', read_name+'.clean.fastq', riboseq_adapters), shell=True)
-
+    
     # Map clean reads to ribosome sequence by bowtie
     subprocess.call('bowtie -p {} -norc --un {} {} {} > {}.map_to_rRNA.sam'
                     .format(thread, without_rrna_reads, tmp_file_location+'/'+ribo_name, read_name+'.clean.fastq', ribo_name),
                     shell=True)
-
+    print('-' * 100)
     print(get_time(), 'Finished clean process.')
+    print('-' * 100)
     global cleanreads
     cleanreads = without_rrna_reads
 
     genome_name = str(genome).split('/')[-1].split('.')[0]
     print(get_time(), 'Start mapping...')
     print('command:')
-    print('tophat2 -p {} -I 10 -o {} {} {}'
+    print('tophat2 -p {} -I 1 -o {} {} {}'
                     .format(thread, tmp_file_location+'/'+read_name+'_tophat_result', tmp_file_location+'/'+genome_name+'.fa', cleanreads))
-    subprocess.call('tophat2 -p {} -I 1 -o {} {} {}'
+    subprocess.call('tophat2 -p {} -I 10 -o {} {} {}'
                     .format(thread, tmp_file_location+'/'+read_name+'_tophat_result', tmp_file_location+'/'+genome_name+'.fa', cleanreads), shell=True)
     print(get_time(), 'Finished mapping')
     print(get_time(), 'Start analysing...')
@@ -89,7 +91,7 @@ def find_reads_on_junction(tmp_file_location, raw_read):
         else:
             print(merge_result.loc[(merge_result.b < i) & (i < merge_result.c)])
             result = result.append(merge_result.loc[(merge_result.b < i) & (i < merge_result.c)])
-    print(result)
+    #print(result)
     try:
         pickle.dump(result, open('RCRJ_result', 'wb'))
     except:
@@ -99,9 +101,9 @@ def find_reads_on_junction(tmp_file_location, raw_read):
 
 
 def remove_tmp_file():
-    subprocess.call('mkdir reads', shell=True)
+    subprocess.call('mkdir -p reads', shell=True)
     subprocess.call('mv *.clean.without.rRNA.fastq ./reads', shell=True)
-    subprocess.call('mkdir result', shell=True)
+    subprocess.call('mkdir -p result', shell=True)
     subprocess.call('rm *.fastq', shell=True)
 
 
@@ -117,9 +119,11 @@ def filter_and_map_reads():
     deal_raw_data()
     remove_tmp_file()
     stop = time.time()
+    print('-' * 100)
     print(get_time(), 'Finished all pipline')
     hours = int(int((stop - start) / 60) / 60)
     print(get_time(), 'Totally run', hours, 'hours')
+    print('-' * 100)
 
 
 def main():
