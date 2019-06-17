1421bcf7f990a08688902bfe0574a9dba31ec444
Peisen Sun <sps@snnu.edu.cn>
2019-06-17 15:48:40 +0800

    Delete Unpublished.py

--- script/Unpublished.py
+++
@@ -1,7 +0,0 @@
-# This software is unpublised
-# We are here to publish the code for editorial review.
-# Please respect our results
-# Thank you! 
-
-# Copyright: Lilab@SNNU
-# E-mail : sps@snnu.edu.cn
