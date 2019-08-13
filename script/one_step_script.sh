4049682f48695eb209170ec0dbb0f966f3be71bf
Peisen Sun <sps@snnu.edu.cn>
2019-08-13 17:08:37 +0800

    Delete one_step_script.sh

--- script/one_step_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yaml = $1
-
-python3 make_virtual_genomes.py -y ${yaml}
-python3 map_to_virtual_genomes.py -y ${yaml}
-python3 find_RCRJ_and_classify.py -y ${yaml}
-python3 find_longest_pep.py -y ${yaml}
