d8647bb19e709d102934842cc31e84b0918f6a26
Peisen Sun <sps@snnu.edu.cn>
2020-07-22 22:39:10 +0800

    Update one_step_script.sh

--- one_step_script.sh
+++ one_step_script.sh
@@ -4,5 +4,4 @@ do
 python3 make_virtual_genomes.py -y config.yaml
 python3 map_to_virtual_genomes.py -y config.yaml
 python3 find_RCRJ_and_classify.py -y config.yaml
-python3 find_longest_pep.py -y config.yaml
 done
